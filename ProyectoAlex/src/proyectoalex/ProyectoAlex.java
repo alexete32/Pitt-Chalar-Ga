cfb09dcf8a028a98731b31c307d3fe2666bae1ba
alexete32 <46757223+alexete32@users.noreply.github.com>
2020-01-17 11:57:41 +0100

    lETS GOOOO
    XD

--- ProyectoAlex/src/proyectoalex/ProyectoAlex.java
+++ ProyectoAlex/src/proyectoalex/ProyectoAlex.java
@@ -11,8 +11,7 @@ public class ProyectoAlex {
 
     
     public static void main(String[] args) {
-        System.out.println("Hello Word"
-                + "");
+        System.out.println("Hello Word");
     }
 
 }
